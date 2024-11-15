6ebac245f278dda4ffdf8e6e43caf1a269c6c05e
Nando Javier Agüero <nandoaguerojs@gmail.com>
2024-11-15 15:58:01 -0300

    inicio del proyecto

---
+++ src/main/java/com/proyectowebsockets/proyecto_web_sockets/ProyectoWebSocketsApplication.java
@@ -0,0 +1,13 @@
+package com.proyectowebsockets.proyecto_web_sockets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoWebSocketsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoWebSocketsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/proyectowebsockets/proyecto_web_sockets/ProyectoWebSocketsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyectowebsockets.proyecto_web_sockets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoWebSocketsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
